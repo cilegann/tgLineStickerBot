e98670be74f4e2121df90b73ff3288b74c27ba06
cilegann <s571991@gmail.com>
2022-02-13 18:45:52 +0800

    fix apng resizing and converting apng -> resized apng -> gif -> webm
    Signed-off-by: cilegann <s571991@gmail.com>

--- animationCvtr.py
+++ animationCvtr.py
@@ -2,17 +2,35 @@
 
 from apnggif import apnggif
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video import fx as vfx
+from PIL import Image, ImageSequence
 
-from moviepy.video.io.ffmpeg_writer import ffmpeg_write_video
+def resizeAnimatedPng(fn):
+    im = Image.open(fn)
+    (w,h) = im.size
+    r=w/h
+    if w>h:
+        newW = 512
+        newH =int(512/r)
+    else:
+        newW = int(512*r)
+        newH = 512
+    frames = []
+    for frame in ImageSequence.Iterator(im):
+        frames.append(frame.resize((newW, newH)))
+    frames[0].save(fn, save_all = True, append_images = frames[1:], duration = 3, loop = 0)
 
 def apng2gif(fn):
     apnggif(fn)
 
-def gif2webm(fn):
+def gif2webm(fn, w=None, h=None, r=None):
     clip = VideoFileClip(fn, has_mask=True)
+    if clip.duration>3:
+        clip = clip.fx(vfx.multiply_speed, final_duration=3)
     clip.write_videofile(fn.replace(".gif", ".webm"), preset="placebo", with_mask=True, ffmpeg_params=['-auto-alt-ref', '0'])
+    clip.close()
 
-def apng2webm(fn):
-    apng2gif(fn)
-    gif2webm(fn.replace(".png", ".webm"))
-    apnggif(fn)
+def apng2webm(pngfn):
+    resizeAnimatedPng(pngfn)
+    apng2gif(pngfn)
+    gif2webm(pngfn.replace(".png", ".gif"))
